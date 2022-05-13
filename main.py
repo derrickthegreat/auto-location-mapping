cadedaafd94d893b9bbc4347b2094e73e139f388
Derrick <derrickcanbereached@gmail.com>
2022-05-12 23:18:34 -0400

    refactor code & add timer to end of iterations

--- main.py
+++ main.py
@@ -2,20 +2,21 @@ import pyautogui
 import pandas
 import time
 
-# File path input
 
-file_path = input('enter file path')
+# File path input
+file_path = input('enter file path:')
+total_rows = input('how many rows?')
 
 if not file_path:
     file_path = 'address-list.xlsx'
 
+
 # Pull data from Excel
-excel_data = pandas.read_excel('personal-budgetting-tool.xlsx')
-total_rows = 100
-count = 0
-time.sleep(2)
+excel_data = pandas.read_excel(file_path)
+
 
 # Iterate through Excel
+count = 0
 for col in excel_data:
     # Enter address
     pyautogui.click(100, 500)
@@ -23,9 +24,11 @@ for col in excel_data:
     pyautogui.press('enter')
     # Clear Search
     pyautogui.click(100, 500)
+    time.sleep(2)
     # End of Data Entry
     if count == total_rows:
         break;
     count += 1
+
 
 print('All Addresses Entered.')
