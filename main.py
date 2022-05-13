7c9b436ca41a3691304f9c32e7adc0571b62aa63
Derrick Alvarez <dalvarez@npuins.com>
2022-05-13 11:19:23 -0400

    refactor loop & add google-earth steps

--- main.py
+++ main.py
@@ -1,34 +1,64 @@
-import pyautogui
-import pandas
-import time
+import pag as pag
+import pandas as pd
+import subprocess
 
 
-# File path input
-file_path = input('enter file path:')
-total_rows = input('how many rows?')
+# User Input
+input_entry_finished = False
 
-if not file_path:
-    file_path = 'address-list.xlsx'
+while not input_entry_finished:
+    # File Path & Name Insured Entry
+    file_path = input('Please enter file path: ')
+    if not file_path:
+        file_path = 'address-list.xlsx'
+    print()
+    named_insured = input('Please enter insureds name: ')
+    print()
+
+    # Review & Confirm
+    print('You have entered:')
+    print('1) File path: ', file_path)
+    print('Named Insured: ', named_insured)
+    finished = input('Is this correct? [Y/n]: ')
+    if finished == 'Y' | finished == 'y' | finished == False:
+        input_entry_finished = True
 
 
 # Pull data from Excel
-excel_data = pandas.read_excel(file_path)
+excel_data = pd.read_excel(file_path)
 
+# Launch Google Earth & set up folder 
+subprocess.Popen("C:\Program Files\Google\Google Earth Pro\client\googleearth.exe")
+pag.keyDown('ctrl')
+pag.keyDown('shift')
+pag.press('n')
+pag.keyUp('ctrl')
+pag.keyUp('shift')
+pag.write(named_insured)
+pag.press('enter')
+pag.press('tab')
 
 # Iterate through Excel
 count = 0
-for col in excel_data:
-    # Enter address
-    pyautogui.click(100, 500)
-    pyautogui.write(excel_data['address'][count])
-    pyautogui.press('enter')
-    # Clear Search
-    pyautogui.click(100, 500)
-    time.sleep(2)
-    # End of Data Entry
-    if count == total_rows:
-        break;
-    count += 1
 
+while excel_data['address'][count]:
+    address = excel_data['address'][count]
+    # Enter address
+    pag.write(address)
+    pag.press('enter')
+    # Save / Drop Pin
+    pag.keyDown('ctrl')
+    pag.keyDown('shift')
+    pag.press('p')
+    pag.keyUp('ctrl')
+    pag.keyUp('shift')
+    pag.write(address)
+    pag.press('enter')
+    # Clear Search
+    pag.keyDown('ctrl')
+    pag.press('a')
+    pag.keyUp('ctrl')
+    # End of Data Entry
+    count += 1
 
 print('All Addresses Entered.')
