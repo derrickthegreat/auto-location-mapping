dc86d2d213f32d2c76392f1a427ec761d845580a
Derrick <derricks@laptop-win32>
2022-05-17 07:51:04 -0400

    added progress dependency

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     version='1.2.4',
     author='Derrick Alvarez',
     author_email='derrickcanbereached@gmail.com',
-    install_requires=['pandas','pyautogui', 'openpyxl'],
+    install_requires=['pandas','pyautogui', 'openpyxl','progress'],
     entry_points='''
         [console_scripts]
         automap=automap:main
